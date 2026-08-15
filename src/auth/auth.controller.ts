c168d7854742b49f9d3a1e4576d58c597e090dd8
Romicha Parvin <romichaparvin35@gmail.com>
2026-08-15 21:44:31 +0600

    login success api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    login(@Body() dto: LoginDto) {
+        return this.authService.login(dto);
+    }
+
+    @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getMe() {
+    return {
+      message: 'You are authenticated',
+    };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async login(dto: LoginDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {email: dto.email,},
+        });
+
+        if(!user) {
+            throw new UnauthorizedException("Invalid email or password")
+        }
+
+       const passwordMatched = await bcrypt.compare(
+      dto.password,
+      user.password,
+    );
+
+        if(!passwordMatched) {
+            throw new UnauthorizedException("Invalid email or password")
+        }
+
+        const payload = {
+            sub: user.id,
+            email: user.email,
+            role: user.role,
+        };
+
+       const accessToken = this.jwtService.sign(payload);
+
+        return {
+            message: "Login successful",
+            accessToken,    
+            user: {
+                id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                role: user.role,
+            },
+        };
+
+    }
+}
