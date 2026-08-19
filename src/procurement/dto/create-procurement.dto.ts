815690ebc8fd3f4d7421b6b50f19b29b13a25309
Romicha Parvin <romichaparvin35@gmail.com>
2026-08-19 21:32:06 +0600

    schema procurement add

---
+++ src/procurement/dto/create-procurement.dto.ts
@@ -0,0 +1,68 @@
+import {
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+enum AttachmentType {
+  INVOICE = 'INVOICE',
+  RECEIPT = 'RECEIPT',
+}
+
+class VoucherDto {
+  @IsString()
+  accountName: string;
+
+  @IsString()
+  accountNumber: string;
+
+  @IsString()
+  bankName: string;
+
+  @IsUUID()
+  @IsOptional()
+  verifiedById?: string;
+
+  @IsUUID()
+  @IsOptional()
+  approvedById?: string;
+}
+
+export class CreateProcurementDto {
+  @IsString()
+  item: string;
+
+  @IsNumber()
+  @Min(1)
+  quantity: number;
+
+  @IsNumber()
+  @Min(0)
+  unitPrice: number;
+
+  @IsUUID()
+  sentToId: string;
+
+  @IsBoolean()
+  @IsOptional()
+  hasAttachment?: boolean;
+
+  @IsEnum(AttachmentType)
+  @IsOptional()
+  attachmentType?: AttachmentType;
+
+  @IsString()
+  @IsOptional()
+  attachmentUrl?: string;
+
+  @ValidateNested()
+  @Type(() => VoucherDto)
+  @IsOptional()
+  voucher?: VoucherDto;
+}
