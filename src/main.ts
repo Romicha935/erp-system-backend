8f12ee0e3b5988f384e4e4b7665135e9f540fa4d
Romicha Parvin <romichaparvin35@gmail.com>
2026-08-26 15:06:07 +0600

    fix: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://erp-system-hr.vercel.app/',
+      'https://erp-system-hr.vercel.app',   
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
